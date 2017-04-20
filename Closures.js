f270cdf21766e6aaaa54266e40edc1d0054663c6
Ryan <ryan@rtpdesign.com>
2017-04-19 21:05:24 -0600

    Update Closures.js

--- Closures.js
+++ Closures.js
@@ -1,6 +1,9 @@
 // 
+// Creates object in memory
 var a = {};
+// for loop creates var x points it 0, 
 for(var x = 0; x < 3; x++){
+  //Creates a function 
   (function (j){
    a[j] = function(){
      return (j)
